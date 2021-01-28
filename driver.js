cd30e1972828ab88525327bc1a0b05ecc0260ec2
chenc19940823 <76268688+chenc19940823@users.noreply.github.com>
2021-01-28 12:11:37 -0800

    Update driver.js
    Added Animation for 'JUMP'

--- driver.js
+++ driver.js
@@ -3,8 +3,8 @@ class Driver {
 		// Constants
 		this.RUN_SPEED = 3;
 		this.PIVOT_SPEED = 3;
+		this.JUMP_SPEED = 5;
 		this.WIDTH = 19;	// Square animation box, HEIGHT == WIDTH
-		
 		// Assign Object Variables
 		Object.assign(this, { game, x, y });
 		this.direction = 0; // 0 - 359, with 0 = right 
@@ -17,6 +17,8 @@ class Driver {
 			this.WIDTH, this.WIDTH, 5, 0.3, 1, this.direction, false, true);	// Standing
 		this.running = new AngleAnimator(this.spritesheet, 0, 0,
 			this.WIDTH, this.WIDTH, 12, 0.05, 1, this.direction, false, true);	// Running
+		this.jumping = new AngleAnimator(this.spritesheet, 0, 21,
+			this.WIDTH, this.WIDTH, 5, 0.3, 1, this.direction, false, true);	// Jumping
 	};
 	
 	update() {
@@ -45,6 +47,11 @@ class Driver {
 			} else if (this.game.backward) {
 				this.x -= ((this.RUN_SPEED / 2) * Math.cos((Math.PI / 180) * this.direction));
 				this.y -= ((this.RUN_SPEED / 2) * Math.sin((Math.PI / 180) * this.direction));
+			}
+			// Jump
+			if(this.game.jump){
+				this.x += (this.JUMP_SPEED * Math.cos((Math.PI / 180) * this.direction));
+				this.y += (this.JUMP_SPEED * Math.sin((Math.PI / 180) * this.direction));
 			}
 		}
 	};
